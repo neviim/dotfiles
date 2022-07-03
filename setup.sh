2250c33d2f15eac28d5c23abbdaecd1c2f58a378
Julio César <zzantares@gmail.com>
2022-07-03 16:45:22 -0500

    Updating local configuration
    For the following tools: GHCi, Tmux, Alacritty, Git and Buildah

--- setup.sh
+++ setup.sh
@@ -5,7 +5,11 @@ this=`dirname $self`
 
 # Create symlinks so config appears in right folder
 ln -sT $this/.lesskey $HOME/.lesskey
+ln -sT $this/.aliases $HOME/.aliases
+ln -sT $this/tmux $HOME/.config/tmux
+ln -sT $this/.ghci $HOME/.ghci
 ln -sT $this/neovim $HOME/.config/nvim
 ln -sT $this/git $HOME/.config/git
 ln -sT $this/bat $HOME/.config/bat
 ln -sT $this/alacritty $HOME/.config/alacritty
+ln -sT $this/containers $HOME/.config/containers
